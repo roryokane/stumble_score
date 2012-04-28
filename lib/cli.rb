6366b4a8d34b2315b0acd6241aa45f1d20394d8e
Rory O’Kane <rory@neomindlabs.com>
2012-04-28 15:33:44 -0400

    wrote step 2 (repeat address), including refactoring to help with output
    CLI.run expects me to return output, instead of puts it. I don't want to have `output += "blah"` everywhere, so I wrote a class with an instance variable that lets me just use `print` and `println` methods. I pass the argument list through with *args so I only have to write it once.

--- lib/cli.rb
+++ lib/cli.rb
@@ -2,8 +2,34 @@ require File.expand_path(File.join('..', 'stumble_score'), __FILE__)
 
 class CLI
 
-  def self.run(address="")
-    "Welcome to StumbleScore."
+  def self.run(*args)
+  	cli_with_output = CLIWithOutput.new
+    return cli_with_output.run(*args)
   end
 
+end
+
+class CLIWithOutput
+	def initialize()
+		@output = ""
+	end
+	
+	def run(*args)
+		run_ready_to_output(*args)
+		return @output
+	end
+
+	def run_ready_to_output(address="")
+		println "Welcome to StumbleScore."
+		println "The address you entered:"
+		println address
+	end
+
+	def print(string)
+		@output += string
+	end
+
+	def println(string)
+		print string + "\n"
+	end
 end
