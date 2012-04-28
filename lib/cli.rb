c411c2410d24b969453ea05fa259d5be8dd18883
Rory O’Kane <rory@neomindlabs.com>
2012-04-28 15:40:49 -0400

    replaced tabs with spaces for consistency
    I prefer tabs, but I don’t want to go through all of the existing files and convert them, and that would make it hard to merge changes with the original repository, anyway.

--- lib/cli.rb
+++ lib/cli.rb
@@ -3,33 +3,33 @@ require File.expand_path(File.join('..', 'stumble_score'), __FILE__)
 class CLI
 
   def self.run(*args)
-  	cli_with_output = CLIWithOutput.new
+    cli_with_output = CLIWithOutput.new
     return cli_with_output.run(*args)
   end
 
 end
 
 class CLIWithOutput
-	def initialize()
-		@output = ""
-	end
-	
-	def run(*args)
-		run_ready_to_output(*args)
-		return @output
-	end
+  def initialize()
+    @output = ""
+  end
+  
+  def run(*args)
+    run_ready_to_output(*args)
+    return @output
+  end
 
-	def run_ready_to_output(address="")
-		println "Welcome to StumbleScore."
-		println "The address you entered:"
-		println address
-	end
+  def run_ready_to_output(address="")
+    println "Welcome to StumbleScore."
+    println "The address you entered:"
+    println address
+  end
 
-	def print(string)
-		@output += string
-	end
+  def print(string)
+    @output += string
+  end
 
-	def println(string)
-		print string + "\n"
-	end
+  def println(string)
+    print string + "\n"
+  end
 end
